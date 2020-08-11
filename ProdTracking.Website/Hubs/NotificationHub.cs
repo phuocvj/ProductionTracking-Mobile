943be1a335797f19cb5f1f44f39a3bd1195eeb28
phuocvj <lebaokhaky@gmail.com>
2020-08-11 16:26:00 +0700

    Count UserOnline

--- ProdTracking.Website/Hubs/NotificationHub.cs
+++ ProdTracking.Website/Hubs/NotificationHub.cs
@@ -1,39 +1,47 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 using ProdTracking.Website.Models;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Web;
 
 namespace ProdTracking.Website.Hubs
 {
+    [HubName("NotificationHub")]
     public class NotificationHub : Hub
     {
         private static readonly ConcurrentDictionary<string, UserHubModels> Users =
        new ConcurrentDictionary<string, UserHubModels>(StringComparer.InvariantCultureIgnoreCase);
-
+      
         public override Task OnConnected()
         {
-            string userName = Context.User.Identity.Name;
+            string userName =  Context.User.Identity.Name;
             string connectionId = Context.ConnectionId;
 
             var user = Users.GetOrAdd(userName, _ => new UserHubModels
             {
                 UserName = userName,
                 ConnectionIds = new HashSet<string>()
+
             });
 
             lock (user.ConnectionIds)
             {
                 user.ConnectionIds.Add(connectionId);
+                
                 if (user.ConnectionIds.Count == 1)
                 {
-                    Clients.Others.userConnected(userName);
+                    Clients.Others.userConnected();
                 }
+                Clients.All.UpdateCount(user.ConnectionIds.Count);
             }
-
+            
             return base.OnConnected();
         }
 
@@ -56,11 +64,12 @@ namespace ProdTracking.Website.Hubs
                         Users.TryRemove(userName, out removedUser);
                         Clients.Others.userDisconnected(userName);
                     }
+                    Clients.All.UpdateCount(user.ConnectionIds.Count);
                 }
             }
-
+           
             return base.OnDisconnected(stopCalled);
         }
-        
+
     }
 }
